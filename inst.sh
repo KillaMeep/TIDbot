060e899c0c5876a5c7cd35f32d8bbc1ed3f6092b
KillaMeep <45155241+KillaMeep@users.noreply.github.com>
2022-05-20 21:32:52 -0600

    Update inst.sh

--- inst.sh
+++ inst.sh
@@ -3,7 +3,7 @@ cd TIDbot
 echo BOT_ID= > .env
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
-sudo apt-get install unzip python3-pip
+sudo apt-get install unzip python3-pip xvfb
 sudo rm -rf google-chrome-stable_current_amd64.deb
 pip install -r requirements.txt
 sudo rm -rf requirements.txt
